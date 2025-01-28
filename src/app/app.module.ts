3247b1a5cd5e4d2f600f53c48945eec2afe8a0a3
Jack_Cool <157689310+CarlDolino@users.noreply.github.com>
2025-01-28 17:27:03 +0800

    added angular materials (#1)
    Co-authored-by: carl <carldolino1006@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts / post-create/post.create.components';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import{ MatCardModule } from '@angular/material/card';
+import{ MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
